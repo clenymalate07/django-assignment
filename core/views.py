319c5311903e99eeab72b42e1c758ffd64c078ea
clenymalate07 <cleny.malate@evsu.edu.ph>
2025-09-15 00:20:19 +0800

    Initial commit - Django A1 and A2

---
+++ core/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def hello(request):
+    return HttpResponse("Hello, EVSU!")
+
+def home(request):
+    return render(request, "home.html", {"title": "Home"})
+
+def about(request):
+    return render(request, "about.html", {"title":"About"})
